c137b291433e8ef4c19f011100f0db4578d7d76f
João Pedro Costa <joao21pedronegocios@gmail.com>
2026-09-11 21:30:34 -0300

    publicação do projeto

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import cors from 'cors'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const app = express()
+l
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(express.static(path.join(__dirname, 'views')))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'))
+})
---
+++ controllers/auth.controller.js
@@ -0,0 +1,12 @@
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import pool from '../config/database.js'
+
+export const gerarHashSenha = async (senha)=> bcrypt.hash(senha, 10)
+export const compararSenha = async (senha, hash)=>{ bcrypt.compare(senha, hash)}
+
+export const gerarToken = (payload) => jwt.sign(
+    payload,
+    process.env.JWT_SECRET,
+    {expiresIn: '8h'}
+)
